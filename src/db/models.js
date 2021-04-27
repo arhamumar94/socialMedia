fc38bef5e50992ab13c6c6598d5cfb80935f0f52
arhamumar94 <arhamumar1999@gmail.com>
2021-04-28 00:19:49 +0530

    hey

--- src/db/models.js
+++ src/db/models.js
@@ -21,38 +21,8 @@ const COL_TITLE_DEF={
     type:Sequelize.DataTypes.STRING(140),
     allowNull:false
 } 
-const Users=db.define("user",{
-    id:COL_ID_DEF,
-    username:COL_NAME_DEF
-
-    
-})
-const Posts=db.define('post',{
-    id:COL_ID_DEF,
-    title:COL_TITLE_DEF,
-    body:{
-        type:Sequelize.DataTypes.TEXT,
-        allowNull:false
-    }
-})
-const Comments=db.define('comment',{
-    id:COL_ID_DEF,
-    title:COL_TITLE_DEF,body:{
-        type:Sequelize.DataTypes.TEXT(`tiny`),
-        allowNull:false
-    }
-    
-})
-Users.hasMany(Posts) //one extra column in post is formed with userid(includeing primary key of users)
-Posts.belongsTo(Users)
-Users.hasMany(Comments)
-Comments.belongsTo(Users)
-Posts.hasMany(Comments)
-Comments.belongsTo(Posts)
-
-
 
 
 module.exports={
-    db,Users,Posts,Comments
+    db
 }
