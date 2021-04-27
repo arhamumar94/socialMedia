faacedc603115e57ac84d0422ac8a1fa9b777fe5
arhamumar94 <arhamumar1999@gmail.com>
2021-04-28 00:27:40 +0530

    hey

--- src/server.js
+++ src/server.js
@@ -1,6 +1,6 @@
 const express= require('express')
 require('dotenv').config()
-const {db}=require('./db/models')
+const db =require('./db/models')
 const {usersRoute}=require('./routes/users')
 const {postsRoute}=require('./routes/posts')
 const app=express();
@@ -11,11 +11,12 @@ app.use('/api/posts',postsRoute)
 app.use('/',express.static(__dirname+'/public'))
 const PORT=process.env.PORT||8383;
 console.log(process.env.PORT)
-app.listen(PORT,()=>{
-    console.log(`server listeing at port http://localhost:8383`)
-})
+
 db.sync().then(()=>{
     console.log(process.env.PORT)
+    app.listen(PORT,()=>{
+        console.log(`server listeing at port http://localhost:8383`)
+    })
    
 }).catch((err)=>{
     console.error(new Error(`could not start server`))
